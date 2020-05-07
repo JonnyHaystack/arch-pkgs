3bc891a27795420d17ce99ce5e0d3014b8cfac7e
Jonathan Haylett <jonathan@haylett.dev>
2020-05-07 17:13:03 +0100

    Use autorandr for LightDM display setup

--- pkg/haystack/desktop/lightdm/display_setup.sh
+++ pkg/haystack/desktop/lightdm/display_setup.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr --setprovideroutputsource modesetting NVIDIA-0
-xrandr --output eDP-1-1 --primary --auto --pos 0x312 --output HDMI-1-1 --auto --pos 1366x0
-xrandr --dpi 96
+autorandr --change --force
